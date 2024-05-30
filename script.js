401849a1392efde5c8ad0378206a4fb2742e99d3
Chibuzor Emmanuel <emmanuelchibuzor2005@gmail.com>
2024-05-30 09:44:15 +0100

    Code Refactor in .prettierc & style.css

--- script.js
+++ script.js
@@ -11,7 +11,6 @@ const btnHold = document.querySelector('.btn--hold')
 const player0 = document.querySelector('.player--0')
 const player1 = document.querySelector('.player--1')
 
-
 score0El.textContent = 0
 score1El.textContent = 0
 diceEl.classList.add('hidden')
@@ -20,28 +19,56 @@ const scores = [0, 0]
 let currScore = 0
 let activePlayer = 0
 
+//Switch Player Func
+const switchPlayer = () => {
+   let active = document.getElementById(`current--${activePlayer}`)
+   let score = document.getElementById(`score--${activePlayer}`)
+
+   score.textContent = scores[activePlayer]
+
+   active.textContent = 0
+   activePlayer = activePlayer === 0 ? 1 : 0
+
+   currScore = 0
+
+   player0.classList.toggle('player--active')
+   player1.classList.toggle('player--active')
+}
+
+// Check Score
+const checkScore = () => {
+   if (scores[activePlayer] >= 100) {
+      // Declare Winner
+      let player = document.querySelector(`.player--${activePlayer}`)
+      player.classList.remove('player--active')
+      player.classList.add('player--winner')
+   }
+}
+
 // Rolling Dice Logic
 btnRoll.addEventListener('click', function () {
-    const dice_value = Math.trunc((Math.random() * 6)) + 1
+   const dice_value = Math.trunc(Math.random() * 6) + 1
 
-    diceEl.classList.remove('hidden')
-    diceEl.src = `./img/dice-${dice_value}.png`
+   diceEl.classList.remove('hidden')
+   diceEl.src = `./img/dice-${dice_value}.png`
 
-    let active = document.getElementById(`current--${activePlayer}`)
+   let active = document.getElementById(`current--${activePlayer}`)
 
-    if (dice_value !== 1) {
-        currScore += dice_value
-        active.textContent = `${currScore}` // change in future
-    } else {
-        scores[activePlayer] += currScore
-        active.textContent = 0
-        activePlayer = activePlayer === 0 ? 1 : 0  
-
-        currScore = 0
-
-        player0.classList.toggle('player--active')
-        player1.classList.toggle('player--active')
-    }
-
+   if (dice_value !== 1) {
+      checkScore()
+      currScore += dice_value
+      active.textContent = `${currScore}` // change in future
+   } else {
+      switchPlayer()
+   }
 })
 
+btnHold.addEventListener('click', function () {
+   scores[activePlayer] += currScore
+   checkScore()
+
+   score0El.textContent = scores[0]
+   score1El.textContent = scores[1]
+
+   switchPlayer()
+})
